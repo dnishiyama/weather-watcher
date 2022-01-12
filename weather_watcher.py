a8d3793c7f25d0692edde28bef2cc4144a17499a
Declan Nishiyama (EtyServer) <dnishiyama@gmail.com>
2022-01-12 20:44:05 +0000

    resolve crontab issues

--- weather_watcher.py
+++ weather_watcher.py
@@ -1,8 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
 from credentials import SLACK_WEBHOOK
+import os
+import sys
 
-LAST_UPDATE_FILE_PATH = 'last_update.txt'
+print(os.getcwd(), __file__, sys.argv[0])
+LAST_UPDATE_FILE_PATH = os.path.dirname(os.path.realpath(__file__)) + '/last_update.txt'
 
 NEW_FORECAST = 'There is a new forecast! https://booneweather.com/Forecast/Boone'
 
@@ -26,22 +29,24 @@ def overwrite_file_path(file_path, contents):
     with open(file_path, 'w+') as last_update_file:
         last_update_file.write(contents)
 
-def send_message():
-    requests.post(SLACK_WEBHOOK, json={'text':NEW_FORECAST})
+def send_message(msg):
+	return requests.post(SLACK_WEBHOOK, json={'text':msg})
 
 def main():
 	rays_weather_content = get_rays_weather_content()
-	rays_weather_soup = BeautifulSoup(rays_weather_content.text)
+	rays_weather_soup = BeautifulSoup(rays_weather_content.text, 'lxml')
 	last_update_str = get_last_update_str(rays_weather_soup)
 
 	cached_update_str = read_file_path(LAST_UPDATE_FILE_PATH)
 	if last_update_str == cached_update_str:
-			print('Forecast has been seen')
+		print('Forecast has been seen')
 	else:
-			print('New forecast!')
+		print('New forecast!')
+		resp = send_message(NEW_FORECAST)
+		if not resp.ok:
+			raise Exception(resp.text)
+		else:
 			overwrite_file_path(LAST_UPDATE_FILE_PATH, last_update_str)
-			send_message()
-
 
 if __name__ == '__main__':
 	main()
